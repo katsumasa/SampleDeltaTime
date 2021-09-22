97a551d9f22abf7cea8a15bfa938c31708e1a4c7
katsumasa <katsumasa@unity3d.com>
2021-09-22 10:40:45 +0900

    Add files via upload

---
+++ Assets/Scripts/Sample.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Sample : MonoBehaviour
+{
+    string[] selectVsyncCounts = { "0", "1", "2" };
+    int selectVsyncCountIdx;
+
+    string[] targetFrameRates = {"-1","30","45","60","90","120"};
+    int targetFrameRateIdx;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Screen.SetResolution(480, 800, true);
+            
+
+        selectVsyncCountIdx = QualitySettings.vSyncCount;
+        switch (Application.targetFrameRate)
+        {                        
+            case 30:
+                targetFrameRateIdx = 1;
+                break;
+            case 45:
+                targetFrameRateIdx = 2;
+                break;
+            case 60:
+                targetFrameRateIdx = 3;
+                break;
+            case 90:
+                targetFrameRateIdx = 4;
+                break;
+            case 120:
+                targetFrameRateIdx = 5;
+                break;
+            
+            default:
+                targetFrameRateIdx = 0;
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Screen.currentResolution.refreshRate " + Screen.currentResolution.refreshRate + "[Hz]");
+        GUILayout.Label("Time.deltaTime " + Time.deltaTime * 1000.0f + "[ms] " + 1.0f / Time.deltaTime + "[FPS]");
+        GUILayout.Label("Time.smoothDeltaTime " + Time.smoothDeltaTime*1000.0f+"[ms] " + 1.0f / Time.smoothDeltaTime + "[FPS]");
+
+
+        GUILayout.Space(100);
+
+        GUILayout.BeginHorizontal();
+        {
+            GUILayout.Label("VSync");
+            var tmp = selectVsyncCountIdx;
+            selectVsyncCountIdx = GUILayout.SelectionGrid(selectVsyncCountIdx, selectVsyncCounts, 3);
+            if(selectVsyncCountIdx != tmp)
+            {
+                QualitySettings.vSyncCount = selectVsyncCountIdx;
+            }
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        {
+            GUILayout.Label("Target Frame Rate");
+            var tmp = targetFrameRateIdx;
+            targetFrameRateIdx = GUILayout.SelectionGrid(targetFrameRateIdx, targetFrameRates, 6);
+            if(targetFrameRateIdx != tmp)
+            {
+                switch(targetFrameRateIdx)
+                {
+                    case 0:
+                        Application.targetFrameRate = -1;
+                        break;
+
+                    case 1:
+                        Application.targetFrameRate = 30;
+                        break;
+
+                    case 2:
+                        Application.targetFrameRate = 45;
+                        break;
+
+                    case 3:
+                        Application.targetFrameRate = 60;
+                        break;
+
+                    case 4:                    
+                        Application.targetFrameRate = 90;
+                        break;
+
+                    case 5:
+                        Application.targetFrameRate = 120;
+                        break;
+                }
+            }
+        }
+        GUILayout.EndHorizontal();
+
+
+
+        
+    }
+}
